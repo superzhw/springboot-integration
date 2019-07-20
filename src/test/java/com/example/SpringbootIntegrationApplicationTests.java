5f02c529d9c889c5992fe767ddf760282419d7fd
zhanghw_zhangsh@126.com <zhanghw_zhangsh@126.com>
2019-07-20 17:30:23 +0800

    读取配置yml文件 注入到bean中
    Signed-off-by: zhanghw_zhangsh@126.com <zhanghw_zhangsh@126.com>

--- src/test/java/com/example/SpringbootIntegrationApplicationTests.java
+++ src/test/java/com/example/SpringbootIntegrationApplicationTests.java
@@ -1,16 +1,23 @@
 package com.example;
 
+import com.example.entity.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 测试
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootIntegrationApplicationTests {
-
+	@Autowired
+	private Person person;
 	@Test
 	public void contextLoads() {
+		System.out.println(person.toString());
 	}
 
 }
