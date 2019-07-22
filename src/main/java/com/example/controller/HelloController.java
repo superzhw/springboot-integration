b05c9ec6edcb93bb002df7abe437247361ac4064
zhanghw_zhangsh@126.com <zhanghw_zhangsh@126.com>
2019-07-22 18:01:45 +0800

    注解修改
    Signed-off-by: zhanghw_zhangsh@126.com <zhanghw_zhangsh@126.com>

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,10 +1,11 @@
-package com.example.hello;
+package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("test")
 public class HelloController {
     @RequestMapping("/hello")
     @ResponseBody
--- src/test/java/com/example/entity/Person.java
+++ src/test/java/com/example/entity/Person.java
@@ -1,18 +1,30 @@
 package com.example.entity;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+
+
 /**
  * 将配置文件中配置的每一个属性的值，映射到这个组件中
  * @ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
  * prefix = "person":配置文件下的属性一一对应
  * 只有这个组件是容器中的组件，才能容器提供的@ConfigurationProperties功能；
+ * @ConfigurationProperties(prefix = "person")默认从全局配置文件中获取值
+ * @PropertySource(value = {"classpath:person.yml"})加载指定的配置文件
+ * @ImportResource 导入Spring的配置文件 让配置文件里面的内容生效
+ * @Configuration 指明当前类为配置类，就是来替代之前的spring配置文件
+ * @Bean 将方法的返回值添加到容器中，容器中这个组件默认的id就是这个方法名 即给容器中添加组件
  */
+
+@PropertySource(value = {"classpath:person.properties"})
 @Component
 @ConfigurationProperties(prefix = "person")
 public class Person {
